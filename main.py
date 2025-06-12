0f76fb12053c4dee365c180266d0e3b947dc3c76
joelsearcy <searcy.joel@gmail.com>
2025-06-12 15:08:17 -0600

    Add --verbose flag

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ def main():
     client = genai.Client(api_key=api_key)
 
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <prompt>")
+        print("Usage: python3 main.py <prompt> [--verbose]")
         sys.exit(1)
 
-    user_prompt = " ".join(sys.argv[1:])
+    verbose = "--verbose" == sys.argv[-1]
+    user_prompt = sys.argv[1]
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -22,10 +23,12 @@ def main():
         model='gemini-2.0-flash-001',
         contents=messages
     )
-    
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
