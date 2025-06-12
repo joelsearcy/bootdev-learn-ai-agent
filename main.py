456276126b6c1cc14054f1cb156ce9dd6b6edf6a
joelsearcy <searcy.joel@gmail.com>
2025-06-12 14:48:00 -0600

    Convert to track a history of messages (prompts)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -12,10 +13,16 @@ def main():
         print("Usage: python3 main.py <prompt>")
         sys.exit(1)
 
-    prompt = " ".join(sys.argv[1:])
+    user_prompt = " ".join(sys.argv[1:])
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
+        model='gemini-2.0-flash-001',
+        contents=messages
     )
+    
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
