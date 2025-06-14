2e33d297d6b83afd5562b2060fa00fd895d8a3a2
joelsearcy <searcy.joel@gmail.com>
2025-06-14 14:18:22 -0600

    Ignore __pycache__ folders

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+def get_file_content(working_directory, file_path):
+    working_path = Path(working_directory)
+    if not working_path.exists():
+        return f'Error: Working directory "{working_directory}" does not exist'
+    full_file_path = working_path / file_path
+
+    if working_path != full_file_path and working_path.absolute() not in full_file_path.resolve().parents:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not full_file_path.exists() or not full_file_path.is_file():
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    MAX_CHARS = 10000
+    try:
+        with full_file_path.open('r', encoding='utf-8') as file:
+            content = file.read(MAX_CHARS)
+        if file.closed:  # Check if there is more content after reading MAX_CHARS
+            content += f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+    except Exception as e:
+        return f'Error: Error reading file "{file_path}": {str(e)}'
